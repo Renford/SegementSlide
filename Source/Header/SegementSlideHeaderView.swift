2983103e2beb2a3f69a4c71ac66e6fee877e6b00
Renford <renchao@emoney.cn>
2020-09-25 17:04:05 +0800

    [fix] header web tap event

--- Source/Header/SegementSlideHeaderView.swift
+++ Source/Header/SegementSlideHeaderView.swift
@@ -39,24 +39,24 @@ internal class SegementSlideHeaderView: UIView {
         headerView.constraintToSuperview()
         lastHeaderView = headerView
     }
-    
-    internal override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        let view = super.hitTest(point, with: event)
-        guard let segementSlideContentView = segementSlideContentView else {
-            return view
-        }
-        guard let selectedIndex = segementSlideContentView.selectedIndex,
-            let segementSlideContentScrollViewDelegate = segementSlideContentView.dequeueReusableViewController(at: selectedIndex)
-            else {
-            return view
-        }
-        if view is UIControl {
-            return view
-        }
-        if !(view?.gestureRecognizers?.isEmpty ?? true) {
-            return view
-        }
-        return segementSlideContentScrollViewDelegate.scrollView
-    }
+
+//    internal override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+//        let view = super.hitTest(point, with: event)
+//        guard let segementSlideContentView = segementSlideContentView else {
+//            return view
+//        }
+//        guard let selectedIndex = segementSlideContentView.selectedIndex,
+//            let segementSlideContentScrollViewDelegate = segementSlideContentView.dequeueReusableViewController(at: selectedIndex)
+//            else {
+//            return view
+//        }
+//        if view is UIControl {
+//            return view
+//        }
+//        if !(view?.gestureRecognizers?.isEmpty ?? true) {
+//            return view
+//        }
+//        return segementSlideContentScrollViewDelegate.scrollView
+//    }
     
 }
